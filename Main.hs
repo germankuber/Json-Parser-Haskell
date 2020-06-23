e94f9435a2cbadbc1067c590e1f0d3035fd03bb1
Germán Küber <german.kuber@outlook.com>
2020-06-22 22:05:02 -0300

    First commit

---
+++ Main.hs
@@ -0,0 +1,103 @@
+module Main where
+
+import Data.Char
+import Control.Applicative
+data JsonValue = JsonNull
+                | JsonBool Bool
+                | JsonNumber Integer
+                | JsonString String
+                | JsonArray [JsonValue]
+                | JsonObject [(String,JsonValue)]
+                deriving (Show, Eq)
+
+newtype Parser a = Parser
+    { runParser :: String -> Maybe (String, a) }
+
+instance Functor Parser where
+    fmap f (Parser p) = 
+        Parser $ \input -> do
+          (input',x) <- p input
+          Just (input', f x)
+
+instance Applicative Parser where
+    pure x = Parser $ \input -> Just (input,x)
+    (Parser p1) <*> (Parser p2)=
+        Parser $ \input -> do
+            (input', f) <- p1 input
+            (input'', a) <- p2 input'
+            Just (input'', f a)
+
+instance Alternative Parser  where
+    empty = Parser $ \_ -> Nothing
+    (Parser p1) <|> (Parser p2) = 
+        Parser $ \input -> p1 input <|> p2 input
+
+
+jsonNull :: Parser JsonValue
+jsonNull = (\_ -> JsonNull) <$> stringP "null"
+
+jsonBool :: Parser JsonValue
+jsonBool = f <$> (stringP "true" <|> stringP "false")
+    where f "true" = JsonBool True
+          f "false" = JsonBool False
+          f _       = undefined  
+          
+charP :: Char -> Parser Char
+charP x = Parser f
+    where
+        f [] = Nothing
+        f (y:ys)   
+          | y == x = Just (ys, x)
+          | otherwise = Nothing
+
+
+stringP :: String -> Parser String
+stringP = sequenceA . map charP 
+
+spanP :: (Char -> Bool) -> Parser String
+spanP f =
+    Parser $ \input ->
+        let (token, rest) = span f input
+        in Just (rest, token) 
+
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) =
+    Parser $ \input -> do
+        (input', xs) <- p input
+        if null xs
+            then Nothing
+            else Just (input', xs)
+
+stringLiteral :: Parser String
+stringLiteral = charP '"' *> spanP (/= '"') <* charP '"'
+
+jsonString :: Parser JsonValue
+jsonString = JsonString <$> stringLiteral
+
+jsonNumber :: Parser JsonValue
+jsonNumber = f <$> notNull( spanP isDigit)
+    where f ds = JsonNumber $ read ds
+
+jsonValue :: Parser JsonValue
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray <|> jsonObject
+
+ws :: Parser String
+ws = spanP isSpace
+
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep element = (:) <$> element <*> many (sep *> element)
+    <|> pure []
+
+jsonArray :: Parser JsonValue
+jsonArray = JsonArray <$> (charP '[' *> elements <* charP ']')
+    where elements = sepBy ( ws *> charP ',' <* ws ) jsonValue
+
+
+jsonObject :: Parser JsonValue
+jsonObject = JsonObject <$> (charP '{' *> ws *> sepBy (ws *> charP ',' <* ws ) pair <* ws <* charP '}')
+    where pair = (\key _ value -> (key, value)) <$> stringLiteral <*> (ws *> charP ':' *> ws) <*> jsonValue
+
+main :: IO ()
+main = undefined
+
+
